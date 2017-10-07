4b00226a7dd897edaecd32104faefa211218a32c
Rijesh Augustine <rijesh@ualberta.ca>
2017-10-07 14:32:31 -0600

    Added Quaternion object

--- CamFootprintTester/MainWindow.xaml.cs
+++ CamFootprintTester/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CamFootprintTester
 {
@@ -20,9 +9,89 @@ namespace CamFootprintTester
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool enableSlider = false;
         public MainWindow()
         {
             InitializeComponent();
+            acHeight.Value = 50.0;
+            vfv.Value = 30;
+            hfv.Value = 60;
+            PointCollection pointCol = new PointCollection(4);
+            pointCol.Add(new Point(0, 0));
+            pointCol.Add(new Point(150, 150));
+            pointCol.Add(new Point(300, 150));
+            pointCol.Add(new Point(150, 0));
+            footprint.Points = pointCol;
+            enableSlider = true;
+            genNewFootprint();
+        }
+
+        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (enableSlider)
+                genNewFootprint();
+        }
+
+        private void genNewFootprint()
+        {
+
+            var TR = new Quaternion( hfv.Value / 2,  vfv.Value / 2, 0, true);
+            var TL = new Quaternion(-hfv.Value / 2,  vfv.Value / 2, 0, true);
+            var BR = new Quaternion( hfv.Value / 2, -vfv.Value / 2, 0, true);
+            var BL = new Quaternion(-hfv.Value / 2, -vfv.Value / 2, 0, true);
+            
+            var gimRot = new Quaternion(gimRoll.Value, gimPitch.Value, gimYaw.Value, true);
+            var acRot = new Quaternion(acRoll.Value, acPitch.Value, acYaw.Value, true);
+
+            double r, p, y;
+
+            Quaternion TR1 = acRot.Multiply(gimRot.Multiply(TR));
+            Quaternion TL1 = acRot.Multiply(gimRot.Multiply(TL));
+            Quaternion BR1 = acRot.Multiply(gimRot.Multiply(BR));
+            Quaternion BL1 = acRot.Multiply(gimRot.Multiply(BL));
+
+            var pc = new PointCollection(4);
+            pc.Add(getPointOnGround(TR1));
+            pc.Add(getPointOnGround(TL1));
+            pc.Add(getPointOnGround(BL1));
+            pc.Add(getPointOnGround(BR1));
+            footprint.Points = bindToViewerDimensions(pc);
+        }
+
+        private Point getPointOnGround(Quaternion q)
+        {
+            double r;
+            double p;
+            double y;
+            q.ConvertToEuler(out r, out p, out y, true);
+
+            r = limit(r, -anglelimit.Value, anglelimit.Value);
+            p = limit(p, -anglelimit.Value, anglelimit.Value);
+
+            double dx = acHeight.Value * Math.Tan(q.Radians(r));
+            double dy = acHeight.Value * Math.Tan(q.Radians(p));
+
+            double utmx =  dx * Math.Cos(q.Radians(y)) - dy * Math.Sin(q.Radians(y)) + 150; // 150 is the centre of viewer. (-) sign is required on dy due to viewer
+            double utmy = -dx * Math.Sin(q.Radians(y)) - dy * Math.Cos(q.Radians(y)) + 150; // 150 is the centre of viewer. (-) sign is required on dx due to viewer
+
+            return new Point(utmx , utmy );
+        }
+
+        private PointCollection bindToViewerDimensions(PointCollection pc)
+        {
+            var outpc = new PointCollection(4);
+            foreach(Point p in pc)
+            {
+                outpc.Add(new Point(limit(p.X, 0, 300), limit(p.Y, 0, 300)));
+            }
+            return outpc;
+        }
+
+        public double limit(double value, double min, double max)
+        {
+            if (value < min) { return min; }
+            if (value > max) { return max; }
+            return value;
         }
     }
 }
---
+++ CamFootprintTester/Quaternion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CamFootprintTester
+{
+    public class Quaternion
+    {
+        public double w { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+
+        public Quaternion()
+        {
+
+        }
+
+        public Quaternion(double roll, double pitch, double yaw, bool isDegrees)
+        {
+            if (isDegrees)
+            {
+                roll = Radians(roll);
+                pitch = Radians(pitch);
+                yaw = Radians(yaw);
+            }
+            double cy = Math.Cos(yaw * 0.5);
+            double sy = Math.Sin(yaw * 0.5);
+            double cr = Math.Cos(roll * 0.5);
+            double sr = Math.Sin(roll * 0.5);
+            double cp = Math.Cos(pitch * 0.5);
+            double sp = Math.Sin(pitch * 0.5);
+
+            w = cy * cr * cp + sy * sr * sp;
+            x = cy * sr * cp - sy * cr * sp;
+            y = cy * cr * sp + sy * sr * cp;
+            z = sy * cr * cp - cy * sr * sp;
+        }
+
+        public void ConvertToEuler(out double roll, out double pitch, out double yaw, bool returnDegrees)
+        {
+            // roll (x-axis rotation)
+            double sinr = +2.0 * (w * x + y * z);
+            double cosr = +1.0 - 2.0 * (x * x + y * y);
+            roll = Math.Atan2(sinr, cosr);
+
+            // pitch (y-axis rotation)
+            double sinp = +2.0 * (w * y - z * x);
+            if (Math.Abs(sinp) >= 1)
+                pitch = CopySign(Math.PI / 2, sinp); // use 90 degrees if out of range
+            else
+                pitch = Math.Asin(sinp);
+
+            // yaw (z-axis rotation)
+            double siny = +2.0 * (w * z + x * y);
+            double cosy = +1.0 - 2.0 * (y * y + z * z);
+            yaw = Math.Atan2(siny, cosy);
+
+            if (returnDegrees)
+            {
+                roll = Degrees(roll);
+                pitch = Degrees(pitch);
+                yaw = Degrees(yaw);
+            }
+        }
+
+        public Quaternion Multiply(Quaternion q2)
+        {
+            return new Quaternion()
+            {
+                x = x * q2.w + y * q2.z - z * q2.y + w * q2.x,
+                y = -x * q2.z + y * q2.w + z * q2.x + w * q2.y,
+                z = x * q2.y - y * q2.x + z * q2.w + w * q2.z,
+                w = -x * q2.x - y * q2.y - z * q2.z + w * q2.w
+            };
+        }
+
+        public double CopySign(double x, double y)
+        {
+            return Math.Abs(x) * Math.Sign(y);
+        }
+
+        public double Radians(double deg)
+        {
+            return deg * Math.PI / 180.0;
+        }
+
+        public double Degrees(double rad)
+        {
+            return rad * 180.0 / Math.PI;
+        }
+        
+    }
+}
